e9858d03504fb75959efd99a62e27f5c6b7be716
Greg Gianforcaro <greggian@gmail.com>
2011-05-08 21:46:06 -0400

    Adding initial Camera capture activity
    Enabling the camera permissions and adding
    a functional camera preview/capture activity.
    
    Not especially useful in the emulator, lame sample image.

---
+++ src/com/flugtag/CaptureActivity.java
@@ -0,0 +1,114 @@
+package com.flugtag;
+
+import java.io.IOException;
+
+import android.app.Activity;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+
+public class CaptureActivity extends Activity implements SurfaceHolder.Callback, Camera.PictureCallback{
+	private final String TAG = "CaptureActivity";
+	private Camera camera;
+
+    /**
+     * Handle the Activity create 
+     * 
+     * @see android.app.Activity#onCreate(android.os.Bundle)
+     * @category Activity
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.capture);
+        
+        //Init the SurfaceHolder and listen for changes
+        SurfaceView sv = (SurfaceView) findViewById(R.id.camSurfaceView);
+        SurfaceHolder holder = sv.getHolder();
+        holder.addCallback(this);
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }    
+
+
+	/**
+	 * Handle the SurfaceHolder create callback
+	 * 
+	 * @see android.view.SurfaceHolder.Callback#surfaceCreated(android.view.SurfaceHolder)
+	 * @category SurfaceHolder.Callback
+	 */
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+        // The Surface has been created, acquire the camera and tell it where to draw.
+		camera = Camera.open();
+        try {
+        	camera.setPreviewDisplay(holder);
+        } catch (IOException exception) {
+        	camera.release();
+        	camera = null;
+        	//TODO: Display user message, preview image failed
+            Log.e(TAG, "IOException caused by setPreviewDisplay()", exception);
+        }	
+	}
+
+	/**
+	 * Handle the SurfaceHolder change callback
+	 * 
+	 * @see android.view.SurfaceHolder.Callback#surfaceChanged(android.view.SurfaceHolder, int, int, int)
+	 * @category SurfaceHolder.Callback
+	 */
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+		if(camera != null){
+			Camera.Parameters params = camera.getParameters();
+			params.setPreviewSize(width, height);	
+			camera.setParameters(params);
+			camera.startPreview();
+		}
+	}
+
+
+	/**
+	 * Handle the SurfaceHolder destroy callback
+	 * 
+	 * @see android.view.SurfaceHolder.Callback#surfaceDestroyed(android.view.SurfaceHolder)
+	 * @category SurfaceHolder.Callback
+	 */
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+        // Surface will be destroyed when we return, so stop the preview.
+		if(camera != null){
+			camera.stopPreview();
+			camera.release();
+			camera = null;
+		}
+	}
+
+
+	/**
+	 * Handle button click event
+	 * 
+	 * @param btn The click target button
+	 * @category Button Event
+	 */
+	public void snapBtnClick(View btn){
+		if(camera != null){
+			camera.takePicture(null, null, null, this);
+		}
+	}
+
+
+	/**
+	 * Handle the camera picture taken callback
+	 * 
+	 * @see android.hardware.Camera.PictureCallback#onPictureTaken(byte[], android.hardware.Camera)
+	 * @category Camera.PictureCallback
+	 */
+	@Override
+	public void onPictureTaken(byte[] data, Camera camera) {
+		Log.d(TAG, "onPictureTaken");
+	}
+}
--- src/com/flugtag/MainActivity.java
+++ src/com/flugtag/MainActivity.java
@@ -1,13 +1,32 @@
 package com.flugtag;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
+	
+    /**
+     * Handle the Activity create. 
+     * 
+     * @see android.app.Activity#onCreate(android.os.Bundle)
+     * @category Activity
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+	/**
+	 * Handle button click event
+	 * 
+	 * @param btn The click target button
+	 * @category Button Event
+	 */
+    public void onScanBtnClick(View btn){
+    	Intent intent = new Intent(btn.getContext(), CaptureActivity.class);
+		startActivity(intent);
     }
 }
